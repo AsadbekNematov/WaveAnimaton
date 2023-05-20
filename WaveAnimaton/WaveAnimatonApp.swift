0e88c2eccd8f18b66dc6edf3410a417dfca3d2bd
Asadbek <asadbekn443@gmail.com>
2023-05-20 09:23:26 -0400

    Initial Commit

---
+++ WaveAnimaton/WaveAnimatonApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaveAnimatonApp.swift
+//  WaveAnimaton
+//
+//  Created by Asadbek Nematov on 5/20/23.
+//
+
+import SwiftUI
+
+@main
+struct WaveAnimatonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
